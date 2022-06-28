a65cf117973af6ea2d00f5a4072ee27a21c99517
Annabella Putri Dirgo <annabellaputridirgo@gmail.com>
2022-06-28 19:58:34 +0700

    feat: parsing JSON file

--- handlers/post.go
+++ handlers/post.go
@@ -1,26 +1,45 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"schedulearn/docker"
+	"os"
 )
 
 type Post struct {
 	Logger *log.Logger
 }
 
+type Message struct {
+	Message string `json:"message"`
+}
+
 func NewPost(logger *log.Logger) *Post {
 	return &Post{logger}
 }
 
+
 func (post *Post) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK) // 200 OK
-	post.Logger.Println("Received request for 'POST'")
-	// read json file from the request body
-	data, _ := ioutil.ReadAll(r.Body)
-	post.Logger.Println("File read: ", string(data))
 
-	dm := docker.NewDockerManager()
+	fileContent, err := os.Open("message.json")
+
+	if err != nil {
+	   log.Fatal(err)
+	   return
+	}
+ 
+	fmt.Println("The File is opened successfully...")
+ 
+	defer fileContent.Close()
+
+   byteResult, _ := ioutil.ReadAll(fileContent)
+
+   var message Message
+
+   json.Unmarshal(byteResult, &message)   
+	post.Logger.Println(message.Message)	
 }
