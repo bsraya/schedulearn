1233b3ac5bb498e8756c03a64a2d75a950fe6689
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-03-25 22:34:23 +0800

    chore: change port

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from app import app
 from app.api.models import create_db_and_tables
+from dotenv import dotenv_values
 
 @app.on_event("startup")
 async def startup_event():
   create_db_and_tables()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, log_level="info", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, log_level="info", port=int(dotenv_values(".env").get("PORT")))
