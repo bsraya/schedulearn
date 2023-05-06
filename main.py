bcae358a42f042eb3f2e3d58f68ab37f63d06a83
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-05-07 00:42:13 +0800

    feat: seed database upon the first initialization

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from app import app
-from app.api.models import create_db_and_tables
+from app.api.db import create_db_and_tables, save_system_settings
 from dotenv import dotenv_values
 
 @app.on_event("startup")
 async def startup_event():
-  create_db_and_tables()
+  create_db_and_tables() 
+  save_system_settings()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, log_level="info", port=int(dotenv_values(".env").get("PORT")))
